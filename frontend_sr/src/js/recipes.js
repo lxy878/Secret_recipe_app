73324fc342536ecbdb8941ed7aaf7e2727f7eeeb
lxy878 <lxy878@msn.cn>
2020-09-18 21:44:46 -0400

    Merge index and show

--- frontend_sr/src/js/recipes.js
+++ frontend_sr/src/js/recipes.js
@@ -6,14 +6,34 @@ class Recipes{
 
     getRecipes(){
         this.server.fetchForRecipes().then(json=>{
-            return json.forEach(recipe => this.recipes.push(new Recipe(recipe)));
+            json.forEach(recipe => this.recipes.push(new Recipe(recipe)));
         }).then(()=>{
             this.render()
         })
     }
 
     render(){
-        this.recipes.forEach(recipe=> console.log(recipe))
+        const recipeContainer = document.getElementById('recipes_container')
+        const recipe = this.recipes[0]
+        this.recipes.forEach(recipe =>{
+            const divTag = document.createElement('div')
+            divTag.id = 'item'
+            divTag.innerHTML = `
+                <img src=${recipe.imageUrl} alt="Image">
+                <div id='item-detail'>
+                    <h4 id='word_wrap'>${recipe.name}</h4>
+                    <p>serving: ${recipe.serving}</p>
+                    <p>meal: ${recipe.meal.name}</p>
+                    <button>View</button>
+                    <button>Delete</button>
+                </div>
+            `
+            // addEventListener for view and delete
+            recipeContainer.appendChild(divTag);
+        })
     }
     
+    getRecipe(){
+        console.log()
+    }
 }
